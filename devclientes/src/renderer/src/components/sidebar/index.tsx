e1e1a0cce124b8d6f57c84a2639d7a7fa0485971
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-03-27 21:47:00 -0300

    now

--- devclientes/src/renderer/src/components/sidebar/index.tsx
+++ devclientes/src/renderer/src/components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 export function Sidebar(){
     return(
         <div>
-            <h1 className="text-white">dddddddd</h1>
+            <h1 className="text-white">Sidebar</h1>
         </div>
     )
-}
+} 
