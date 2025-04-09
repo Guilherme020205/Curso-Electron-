6bb8307c0c50f4d665ebd888ac0b422f4e4b8552
Guilherme020205 <seemannguilherme@gmail.com>
2025-04-09 17:12:59 -0300

    cadastro de cliente ok

--- devclientes/src/renderer/src/pages/create.tsx
+++ devclientes/src/renderer/src/pages/create.tsx
@@ -1,26 +1,131 @@
-export function Create(){
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { FormEvent, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-    async function handleAddCustomer() {
+interface DataMutation{
+    name: string,
+    address: string,
+    email: string,
+    phone: string,
+    role: string
+}
 
-        const doc = {
-            name: "jlljljljllherme",
-            email: "jlljljljl@gmail.com",
-            phone: "444444444",
-            address: "Rua x sfsdcentro",
-            role: "Dev front-edddddddnd",
-            status: true
-        }
+export function Create() {
 
+    const navigate = useNavigate()
 
-        const response = await window.api.addCustomer(doc)
-        console.log(response)
+  const queryClient = useQueryClient()
+  const nameRef = useRef<HTMLInputElement | null>(null)
+  const emailRef = useRef<HTMLInputElement | null>(null)
+  const phoneRef = useRef<HTMLInputElement | null>(null)
+  const addressRef = useRef<HTMLInputElement | null>(null)
+  const roleRef = useRef<HTMLInputElement | null>(null)
+
+  const {isPending, mutateAsync: creatCustomer} = useMutation({ mutationFn: async (data: DataMutation) => {
+    
+    await window.api.addCustomer({
+        name: data.name,
+        address: data.address,
+        email: data.email,
+        phone: data.phone,
+        role: data.role,
+        status: true
+    }).then((response) => {
+        // console.log(response)
+        navigate('/')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+  }, 
+  onSuccess: () => {
+    queryClient.invalidateQueries({queryKey: ["customers"]})
+  }
+})
+
+  async function handleAddCustomer(e: FormEvent) {
+    e.preventDefault()
+
+    const name = nameRef.current?.value
+    const address = addressRef.current?.value
+    const email = emailRef.current?.value
+    const phone = phoneRef.current?.value
+    const role = roleRef.current?.value
+
+    if (!name || !address || !email || !phone || !role) {
+      return
     }
+    await creatCustomer({
+        name,
+        address,
+        email,
+        phone,
+        role
+    })
+  }
 
-    return(
-        <>
-            <h1>Create</h1>
+  return (
+    <div className="flex-1 flex flex-col py-12 px-10 gap-8 overflow-y-auto">
+      <section className="flex flex-1 flex-col items-center">
+        <h1 className="text-white font-semibold text-xl lg:text-3xl ">Cadastro novo cliente</h1>
 
-            <button onClick={handleAddCustomer}>Cadastro</button>
-        </>
-    )
+        <form className="w-full max-w-96 mt-4" onSubmit={handleAddCustomer}>
+          <div className="mb-2">
+            <label className="text-lg">Nome:</label>
+            <input
+              type="text"
+              placeholder="Digite nome do cliente..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={nameRef}
+            />
+          </div>
+          <div className="mb-2">
+            <label className="text-lg">Endereço:</label>
+            <input
+              type="text"
+              placeholder="Digite endereço completo..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={addressRef}
+            />
+          </div>
+          <div className="mb-2">
+            <label className="text-lg">Email:</label>
+            <input
+              type="text"
+              placeholder="Digite email..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={emailRef}
+            />
+          </div>
+          <div className="mb-2">
+            <label className="text-lg">Telefone:</label>
+            <input
+              type="text"
+              placeholder="Digite elefone..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={roleRef}
+            />
+          </div>
+          <div className="mb-8">
+            <label className="text-lg">Cargo:</label>
+            <input
+              type="text"
+              placeholder="Digite o cargo..."
+              className="w-full h-9 rounded text-black px-2"
+              ref={phoneRef}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-blue-500 rounded flex items-center justify-center w-full h-9 disabled:bg-gray-500"
+            disabled={isPending}
+          >
+            Cadastrar
+          </button>
+        </form>
+      </section>
+    </div>
+  )
 }
